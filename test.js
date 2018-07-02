d5ed531e7ce7765175b52521f9ccd25390829b7d
sunhongpeng <sunhongpeng@gridsum.com>
2018-07-02 18:47:54 +0800

    Add：增加测试用例

--- test.js
+++ test.js
@@ -9,105 +9,47 @@
 
 require('mocha');
 var assert = require('assert');
-var isNumber = require('./');
+var hasEmptyProperty = require('./');
 
-describe('is a number', function() {
+describe('has a empty property', function() {
   var fixtures = [
-    0xff,
-    5e3,
-    0,
-    0.1,
-    -0.1,
-    -1.1,
-    37,
-    3.14,
-
-    1,
-    1.1,
-    10,
-    10.1,
-    100,
-    -100,
-
-    '0.1',
-    '-0.1',
-    '-1.1',
-    '0',
-    '012',
-    '0xff',
-    '1',
-    '1.1',
-    '10',
-    '10.10',
-    '100',
-    '5e3',
-    '   56\r\n  ', // issue#3
-
-    Math.LN2,
-
-    // 012, Octal literal not allowed in strict mode
-    parseInt('012'),
-    parseFloat('012'),
-    Math.abs(1),
-    Math.acos(1),
-    Math.asin(1),
-    Math.atan(1),
-    Math.atan2(1, 2),
-    Math.ceil(1),
-    Math.cos(1),
-    Math.E,
-    Math.exp(1),
-    Math.floor(1),
-    Math.LN10,
-    Math.LN2,
-    Math.log(1),
-    Math.LOG10E,
-    Math.LOG2E,
-    Math.max(1, 2),
-    Math.min(1, 2),
-    Math.PI,
-    Math.pow(1, 2),
-    Math.pow(5, 5),
-    Math.random(1),
-    Math.round(1),
-    Math.sin(1),
-    Math.sqrt(1),
-    Math.SQRT1_2,
-    Math.SQRT2,
-    Math.tan(1),
-
-    Number.MAX_VALUE,
-    Number.MIN_VALUE,
-
-    '0.0',
-    '0x0',
-    '0e+5',
-    '000',
-    '0.0e-5',
-    '0.0E5',
-
-    +'',
-    +1,
-    +3.14,
-    +37,
-    +5,
-    +[],
-    +false,
-    +Math.LN2,
-    +true,
-    +null,
-    +Buffer.from(''),
-    +new Date()
+    {
+      'a': '',
+    },
+    {
+      'b': null,
+    },
+    {
+      'c': undefined,
+    },
+    {
+      'd': new Array(),
+    },
+    {
+      'e': NaN,
+    },
+    {
+      'e1': Infinity,
+    },
+    {
+      'f': /foo/
+    },
+    {
+      'g': false
+    },
+    {
+      'g': function() {}
+    }
   ];
 
-  fixtures.forEach(function(num, idx) {
-    it(JSON.stringify(num) + ' should be a number', function() {
-      assert(isNumber(num), 'expected "' + String(num) + '" to be a number');
+  fixtures.forEach(function(obj, idx) {
+    it(JSON.stringify(obj) + ' should has a empty property', function() {
+      assert(hasEmptyProperty(obj), 'expected "' + String(obj) + '" has a empty property');
     });
   });
 });
 
-describe('is not a number', function() {
+describe('has not a empty property', function() {
   var fixtures = [
     '   ', // issue#3
     '\r\n\t', // issue#3
@@ -146,9 +88,10 @@ describe('is not a number', function() {
     {}
   ];
 
-  fixtures.forEach(function(num) {
-    it(JSON.stringify(num) + ' should not be a number', function() {
-      assert(!isNumber(num), 'expected "' + String(num) + '" to not be a number');
+  fixtures.forEach(function(item, index) {
+    var obj = { [index]: item };
+    it(JSON.stringify(obj) + ' should not have a empty propert', function() {
+      assert(!hasEmptyProperty(obj), 'expected "' + String(obj) + '" not has a empty propert');
     });
   });
 });
